7054463ae8ed2e55f0bc4ab6d45e152610d34252
Kishalay Ray <kishalayray13@gmail.com>
2021-06-05 12:41:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React,{useState,useEffect} from 'react'
+import './App.css'
+import Loader from './components/Loader';
+import Map from './components/Map';
+import Header from './components/Header';
+const App = () => {
+  const [eventData,setEventData]=useState([]);
+  const [loading,setLoading]=useState(false);
+  useEffect(()=>{
+    const fetchEvents=async()=>{
+      setLoading(true)
+      const res=await fetch('https://eonet.sci.gsfc.nasa.gov/api/v2.1/events');
+      const {events}=await res.json();
+      setEventData(events)
+      setLoading(false)
+    }
+    fetchEvents();
+    console.log(eventData)
+  },[])
+
+  return (
+    <div>
+    <Header/>
+      { !loading ? <Map eventData={eventData}/> : <Loader/>}
+    </div>
+  )
+}
+
+export default App    
+  
